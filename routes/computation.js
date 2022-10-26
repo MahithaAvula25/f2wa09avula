1414a57d494bb7fca38b1e7005829337ae2d7731
MahithaAvula25 <avula.usedu2@gmail.com>
2022-10-26 18:13:39 -0500

    Added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -2,10 +2,10 @@ var express=require('express');
 var router = express.Router();
 
 router.get('/',function(req,res,next){
-   var number1=Math.floor(Math.random())
-   var number2=Math.floor(Math.random())
+   var number1=Math.random()*20
+   var number2=Math.random()
    var number3=Math.floor(Math.random()*20)
-   var number4=Math.floor(Math.random())
+   var number4=Math.random()*-10
    var sinfunction=Math.sin(number1);
    var acosfunction=Math.acos(number2);
    var sinhfunction=Math.sinh(number3);
