366d01272dec9f2bd21c5dcde90391ccbb84613e
MahithaAvula25 <avula.usedu2@gmail.com>
2022-10-25 19:16:29 -0500

    Added bonus

---
+++ routes/computation.js
@@ -0,0 +1,32 @@
+var express=require('express');
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+   var number1=Math.floor(Math.random())
+   var number2=Math.floor(Math.random())
+   var number3=Math.floor(Math.random()*20)
+   var number4=Math.floor(Math.random())
+   var sinfunction=Math.sin(number1);
+   var acosfunction=Math.acos(number2);
+   var sinhfunction=Math.sinh(number3);
+   var absfunction=Math.abs(number4);
+   res.render('computation',
+   {
+    title:'Mahitha Avula',
+    number1:number1,
+    number2:number2,
+    number3:number3,
+    number4:number4,
+    acos:acosfunction,
+    sinh:sinhfunction,
+    abs:absfunction,
+    sin:sinfunction,
+
+
+   });
+   
+});
+
+
+
+module.exports=router;
